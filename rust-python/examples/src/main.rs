0ee73998d10d0391f72fe1bdc3179d13643be24a
ismail codar <icodar@gmail.com>
2021-11-26 13:44:05 +0300

    src/freeze.py

--- rust-python/examples/src/main.rs
+++ rust-python/examples/src/main.rs
@@ -1,7 +1,9 @@
-mod hello_embed;
+// mod hello_embed;
+mod freeze;
 
 fn main() {
-    let result = hello_embed::main();
+    // let result = hello_embed::main();
+    let result = freeze::main();
     if let Err(_e) = result {
         eprintln!("hata");
     }
